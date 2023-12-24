dbb89343ba06290fd2b5b901f1006fe47a9ae745
Manuel Marquez <mmarquezlop.99@gmail.com>
2023-12-23 19:27:51 -0600

    Added a test file to check js code and syntax

---
+++ 201-notes/testing_welcome_message.js
@@ -0,0 +1,12 @@
+realUserAccount = 1;
+welcome_Message = ["Welcome!!! 🎈"];
+
+if (realUserAccount) {
+  console.log("You are a real user! Please explore our platform");
+  {
+    for (let i = 0; i < 2000; i++) { // Welcome message is displayed 2000 times
+      console.log(welcome_Message);
+    }
+  }
+
+}
